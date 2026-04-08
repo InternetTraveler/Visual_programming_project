241d96c913ad0a2d2a404612b3df171c328cd296
gitTraveler <122952539+InternetTraveler@users.noreply.github.com>
2026-04-08 21:03:44 +0700

    #3 Создаие null ошибок для объектов

---
+++ VisualProgrammingProgramm/Domain/VisualProgramming.Domain/VisualProgramming.Domain/Exceptions/NullExeption/ModulNullExeption.cs
@@ -0,0 +1,7 @@
+﻿namespace VisualProgramming.Domain.Exceptions.NullExeption;
+
+public class ModulNullExeption : BaseNullExeption
+{
+    public ModulNullExeption(object Obj, string nameNullObj, Type typeNullObj) 
+        : base(Obj, nameNullObj, typeNullObj) {}
+}
---
+++ VisualProgrammingProgramm/Domain/VisualProgramming.Domain/VisualProgramming.Domain/Exceptions/NullExeption/PortNullConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace VisualProgramming.Domain.Exceptions.NullExeption;
+
+public class PortNullConnection : BaseNullExeption
+{
+    public PortNullConnection(object Obj, string nameNullObj, Type typeNullObj)
+        : base(Obj, nameNullObj, typeNullObj) { }
+}
