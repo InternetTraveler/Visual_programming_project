de100a5975706b8b11bb77abfeb0dec6a1c2b45b
gitTraveler <122952539+InternetTraveler@users.noreply.github.com>
2026-04-09 09:37:37 +0700

    Создание консольного проекта для теста доменого слоя (для галачки) close #4

--- VisualProgrammingProgramm/Domain/VisualProgramming.ValueObject/VisualProgramming.ValueObject/Exeption/LevelOfDepthOutOfRangeExeption.cs
+++ VisualProgrammingProgramm/Domain/VisualProgramming.ValueObject/VisualProgramming.ValueObject/Exeption/LevelOfDepthOutOfRangeExeption.cs
@@ -5,7 +5,7 @@ public class LevelOfDepthOutOfRangeExeption : ArgumentOutOfRangeException
     public readonly int Value;
 
     public LevelOfDepthOutOfRangeExeption(int value)
-        : base("Переменая не может быть меньше нуля")
+        : base("Переменая не может быть меньше или равен нулю")
     {
         Value = value;
     }
