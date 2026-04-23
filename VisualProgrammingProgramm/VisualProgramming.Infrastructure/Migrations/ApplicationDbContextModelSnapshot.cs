528a2a98dba4cabea665e69285af2978a35541ee
gitTraveler <122952539+InternetTraveler@users.noreply.github.com>
2026-04-23 17:53:30 +0700

    Создание миграционых файлов #11

---
+++ VisualProgrammingProgramm/VisualProgramming.Infrastructure/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -0,0 +1,328 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using VisualProgramming.Migrations;
+
+namespace VisualProgramming.Infrastructure.Migrations;
+
+[DbContext(typeof(ApplicationDbContext))]
+partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+{
+    protected override void BuildModel(ModelBuilder modelBuilder)
+    {
+#pragma warning disable 612, 618
+        modelBuilder
+            .HasAnnotation("ProductVersion", "8.0.8")
+            .HasAnnotation("Relational:MaxIdentifierLength", 63);
+
+        NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Project", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<string>("Name")
+                .IsRequired()
+                .HasColumnType("text");
+
+            b.HasKey("Id");
+
+            b.ToTable("Projects");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Graf", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("ProjectId")
+                .HasColumnType("uuid");
+
+            b.HasKey("Id");
+
+            b.HasIndex("ProjectId");
+
+            b.ToTable("Grafs");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.ElementGraf", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<Guid?>("NodeId")
+                .HasColumnType("uuid");
+
+            b.Property<int>("LevelLevelOfDepthOperation")
+                .HasColumnType("integer");
+
+            b.Property<bool>("IsModul")
+                .HasColumnType("boolean");
+
+            b.Property<Guid>("ParentGrafId")
+                .HasColumnType("uuid");
+
+            b.Property<double>("PositionX")
+                .HasColumnType("double precision");
+
+            b.Property<double>("PositionY")
+                .HasColumnType("double precision");
+
+            b.HasKey("Id");
+
+            b.HasIndex("NodeId");
+
+            b.HasIndex("ParentGrafId");
+
+            b.ToTable("ElementGrafs");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.BaseNode", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<string>("Name")
+                .IsRequired()
+                .HasColumnType("text");
+
+            b.Property<string>("Discriminator")
+                .IsRequired()
+                .HasMaxLength(21)
+                .HasColumnType("character varying(21)");
+
+            b.HasKey("Id");
+
+            b.ToTable("Nodes");
+
+            b.HasDiscriminator<string>("Discriminator").HasValue("BaseNode");
+
+            b.UseTphMappingStrategy();
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Port", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("NodeId")
+                .HasColumnType("uuid");
+
+            b.Property<int>("TypePort")
+                .HasColumnType("integer");
+
+            b.Property<string>("Description")
+                .IsRequired()
+                .HasColumnType("text");
+
+            b.HasKey("Id");
+
+            b.HasIndex("NodeId");
+
+            b.ToTable("Ports");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Connection", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("SourcePortId")
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("TargetPortId")
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("InElementGrafId")
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("OutElementGrafId")
+                .HasColumnType("uuid");
+
+            b.HasKey("Id");
+
+            b.HasIndex("SourcePortId");
+
+            b.HasIndex("TargetPortId");
+
+            b.HasIndex("InElementGrafId");
+
+            b.HasIndex("OutElementGrafId");
+
+            b.ToTable("Connections");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.NodePortConnection", b =>
+        {
+            b.Property<Guid>("Id")
+                .ValueGeneratedOnAdd()
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("NodeId")
+                .HasColumnType("uuid");
+
+            b.Property<Guid>("PortId")
+                .HasColumnType("uuid");
+
+            b.HasKey("Id");
+
+            b.HasIndex("NodeId");
+
+            b.HasIndex("PortId");
+
+            b.ToTable("NodePortConnections");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Node", b =>
+        {
+            b.HasBaseType("VisualProgramming.Domain.Entites.BaseNode");
+
+            b.Property<int>("TypeOperation")
+                .HasColumnType("integer");
+
+            b.HasDiscriminator().HasValue("Node");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Modul", b =>
+        {
+            b.HasBaseType("VisualProgramming.Domain.Entites.BaseNode");
+
+            b.Property<Guid>("GrafOperationId")
+                .HasColumnType("uuid");
+
+            b.HasIndex("GrafOperationId");
+
+            b.HasDiscriminator().HasValue("Modul");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Graf", b =>
+        {
+            b.HasOne("VisualProgramming.Domain.Entites.Project", "Project")
+                .WithMany("Grafs")
+                .HasForeignKey("ProjectId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.Navigation("Project");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.ElementGraf", b =>
+        {
+            b.HasOne("VisualProgramming.Domain.Entites.BaseNode", "Node")
+                .WithMany()
+                .HasForeignKey("NodeId");
+
+            b.HasOne("VisualProgramming.Domain.Entites.Graf", "ParentGraf")
+                .WithMany("ElementsGraf")
+                .HasForeignKey("ParentGrafId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.Navigation("Node");
+
+            b.Navigation("ParentGraf");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Port", b =>
+        {
+            b.HasOne("VisualProgramming.Domain.Entites.BaseNode", "Node")
+                .WithMany()
+                .HasForeignKey("NodeId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.Navigation("Node");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Connection", b =>
+        {
+            b.HasOne("VisualProgramming.Domain.Entites.Port", "SourcePort")
+                .WithMany()
+                .HasForeignKey("SourcePortId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.HasOne("VisualProgramming.Domain.Entites.Port", "TargetPort")
+                .WithMany()
+                .HasForeignKey("TargetPortId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.HasOne("VisualProgramming.Domain.Entites.ElementGraf", "InElementGraf")
+                .WithMany()
+                .HasForeignKey("InElementGrafId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.HasOne("VisualProgramming.Domain.Entites.ElementGraf", "OutElementGraf")
+                .WithMany()
+                .HasForeignKey("OutElementGrafId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.Navigation("InElementGraf");
+
+            b.Navigation("OutElementGraf");
+
+            b.Navigation("SourcePort");
+
+            b.Navigation("TargetPort");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.NodePortConnection", b =>
+        {
+            b.HasOne("VisualProgramming.Domain.Entites.BaseNode", "Node")
+                .WithMany("NodePortConnections")
+                .HasForeignKey("NodeId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.HasOne("VisualProgramming.Domain.Entites.Port", "Port")
+                .WithMany("NodePortConnections")
+                .HasForeignKey("PortId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.Navigation("Node");
+
+            b.Navigation("Port");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Modul", b =>
+        {
+            b.HasOne("VisualProgramming.Domain.Entites.Graf", "GrafOperation")
+                .WithMany()
+                .HasForeignKey("GrafOperationId")
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+
+            b.Navigation("GrafOperation");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Project", b =>
+        {
+            b.Navigation("Grafs");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Graf", b =>
+        {
+            b.Navigation("ElementsGraf");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.BaseNode", b =>
+        {
+            b.Navigation("NodePortConnections");
+        });
+
+        modelBuilder.Entity("VisualProgramming.Domain.Entites.Port", b =>
+        {
+            b.Navigation("NodePortConnections");
+        });
+    }
+}
---
+++ VisualProgrammingProgramm/VisualProgramming.Infrastructure/Migrations/Initial.cs
@@ -0,0 +1,309 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VisualProgramming.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Discriminator = table.Column<string>(type: "character varying(21)", maxLength: 21, nullable: false),
+                    TypeOperation = table.Column<int>(type: "integer", nullable: true),
+                    GrafOperationId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grafs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProjectId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grafs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Grafs_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ports",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NodeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TypePort = table.Column<int>(type: "integer", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ports_Nodes_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ElementGrafs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NodeId = table.Column<Guid>(type: "uuid", nullable: true),
+                    LevelLevelOfDepthOperation = table.Column<int>(type: "integer", nullable: false),
+                    IsModul = table.Column<bool>(type: "boolean", nullable: false),
+                    ParentGrafId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PositionX = table.Column<double>(type: "double precision", nullable: false),
+                    PositionY = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ElementGrafs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ElementGrafs_Grafs_ParentGrafId",
+                        column: x => x.ParentGrafId,
+                        principalTable: "Grafs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ElementGrafs_Nodes_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NodePortConnections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    NodeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PortId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NodePortConnections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NodePortConnections_Nodes_NodeId",
+                        column: x => x.NodeId,
+                        principalTable: "Nodes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NodePortConnections_Ports_PortId",
+                        column: x => x.PortId,
+                        principalTable: "Ports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    SourcePortId = table.Column<Guid>(type: "uuid", nullable: false),
+                    TargetPortId = table.Column<Guid>(type: "uuid", nullable: false),
+                    InElementGrafId = table.Column<Guid>(type: "uuid", nullable: false),
+                    OutElementGrafId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Connections_ElementGrafs_InElementGrafId",
+                        column: x => x.InElementGrafId,
+                        principalTable: "ElementGrafs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Connections_ElementGrafs_OutElementGrafId",
+                        column: x => x.OutElementGrafId,
+                        principalTable: "ElementGrafs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Connections_Ports_SourcePortId",
+                        column: x => x.SourcePortId,
+                        principalTable: "Ports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Connections_Ports_TargetPortId",
+                        column: x => x.TargetPortId,
+                        principalTable: "Ports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            // Индексы для оптимизации запросов
+            migrationBuilder.CreateIndex(
+                name: "IX_Grafs_ProjectId",
+                table: "Grafs",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElementGrafs_NodeId",
+                table: "ElementGrafs",
+                column: "NodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ElementGrafs_ParentGrafId",
+                table: "ElementGrafs",
+                column: "ParentGrafId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ports_NodeId",
+                table: "Ports",
+                column: "NodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NodePortConnections_NodeId",
+                table: "NodePortConnections",
+                column: "NodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NodePortConnections_PortId",
+                table: "NodePortConnections",
+                column: "PortId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_SourcePortId",
+                table: "Connections",
+                column: "SourcePortId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_TargetPortId",
+                table: "Connections",
+                column: "TargetPortId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_InElementGrafId",
+                table: "Connections",
+                column: "InElementGrafId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_OutElementGrafId",
+                table: "Connections",
+                column: "OutElementGrafId");
+
+            // Дополнительный индекс для Modul, если нужно ускорить поиск по GrafOperationId
+            migrationBuilder.CreateIndex(
+                name: "IX_Nodes_GrafOperationId",
+                table: "Nodes",
+                column: "GrafOperationId");
+
+            // Внешний ключ для Modul.GrafOperationId (добавляется отдельно, т.к. таблица Nodes уже создана)
+            migrationBuilder.AddForeignKey(
+                name: "FK_Nodes_Grafs_GrafOperationId",
+                table: "Nodes",
+                column: "GrafOperationId",
+                principalTable: "Grafs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Удаление внешнего ключа для Modul
+            migrationBuilder.DropForeignKey(
+                name: "FK_Nodes_Grafs_GrafOperationId",
+                table: "Nodes");
+
+            // Удаление индексов
+            migrationBuilder.DropIndex(
+                name: "IX_Nodes_GrafOperationId",
+                table: "Nodes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Connections_OutElementGrafId",
+                table: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Connections_InElementGrafId",
+                table: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Connections_TargetPortId",
+                table: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Connections_SourcePortId",
+                table: "Connections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NodePortConnections_PortId",
+                table: "NodePortConnections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_NodePortConnections_NodeId",
+                table: "NodePortConnections");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Ports_NodeId",
+                table: "Ports");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ElementGrafs_ParentGrafId",
+                table: "ElementGrafs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ElementGrafs_NodeId",
+                table: "ElementGrafs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Grafs_ProjectId",
+                table: "Grafs");
+
+            // Удаление таблиц
+            migrationBuilder.DropTable(
+                name: "Connections");
+
+            migrationBuilder.DropTable(
+                name: "NodePortConnections");
+
+            migrationBuilder.DropTable(
+                name: "ElementGrafs");
+
+            migrationBuilder.DropTable(
+                name: "Ports");
+
+            migrationBuilder.DropTable(
+                name: "Grafs");
+
+            migrationBuilder.DropTable(
+                name: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
