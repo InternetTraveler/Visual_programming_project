35006795b9bbf5eb16217c421a8fb37a733a9e7c
gitTraveler <122952539+InternetTraveler@users.noreply.github.com>
2026-04-23 17:55:06 +0700

    Доп файлы инфарструктурного слоя #11

---
+++ VisualProgrammingProgramm/VisualProgramming.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VisualProgramming.Domain.Entites;
+
+namespace VisualProgramming.Migrations;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<Graf> Grafs { get; set; }
+    public DbSet<ElementGraf> ElementGrafs { get; set; }
+    public DbSet<Node> Nodes { get; set; }
+    public DbSet<Modul> Moduls { get; set; }
+    public DbSet<Connection> Connections { get; set; }
+    public DbSet<NodePortConnection> NodePortConnections { get; set; }
+    public DbSet<Port> Ports { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.EnableSensitiveDataLogging();
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+    }
+
+
+}
